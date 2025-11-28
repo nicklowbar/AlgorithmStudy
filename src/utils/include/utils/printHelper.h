6b214f683b096845ebdb8cc0ca74f7b79ab60449
Nicholas Barlow <nicklowbar@gmail.com>
2025-11-28 13:38:44 -0800

    Remove g++13 requirement, fix vector printint

--- src/utils/include/utils/printHelper.h
+++ src/utils/include/utils/printHelper.h
@@ -1,53 +1,37 @@
 #pragma once
 
+#include <queue>
+#include <sstream>
+#include <stack>
 #include <string>
 #include <vector>
-#include <stack>
-#include <queue>
 
-namespace utils
-{
-    template <typename...Args>
-    std::string printVector(std::vector<Args...>& vec)
-    {
-        std::string result = "[";
-        for (int i = 0; i < vec.size(); i++) {
-            result += std::to_string(vec[i]);
-            if (i < vec.size() - 1) {
-                result += ", ";
-            }
-        }
-        result += "]";
-        return result;
-    }
+namespace utils {
 
-    template <typename T = std::string>
-    std::string printVector(std::vector<T>& vec)
-    {
-        std::string result = "[";
-        for (int i = 0; i < vec.size(); i++) {
-            result += vec[i];
-            if (i < vec.size() - 1) {
-                result += ", ";
-            }
-        }
-        result += "]";
-        return result;
+template <typename T> std::string printVector(std::vector<T> &vec) {
+  std::ostringstream oss;
+  oss << "[";
+  for (int i = 0; i < vec.size(); i++) {
+    oss << vec[i];
+    if (i < vec.size() - 1) {
+      oss << ", ";
     }
-
-    template <typename...Args>
-    std::string printStack(std::stack<Args...>& stk)
-    {
-        std::string result = "[";
-        std::stack<Args...> temp = stk;
-        while (!temp.empty()) {
-            result += std::to_string(temp.top());
-            temp.pop();
-            if (!temp.empty()) {
-                result += ", ";
-            }
-        }
-        result += "]";
-        return result;
-    }
+  }
+  oss << "]";
+  return oss.str();
 }
+
+template <typename... Args> std::string printStack(std::stack<Args...> &stk) {
+  std::string result = "[";
+  std::stack<Args...> temp = stk;
+  while (!temp.empty()) {
+    result += std::to_string(temp.top());
+    temp.pop();
+    if (!temp.empty()) {
+      result += ", ";
+    }
+  }
+  result += "]";
+  return result;
+}
+} // namespace utils
